bd1be1b87d1ae442064c7da5d469ce09a12cb239
Guilherme Bleidão <guilhermenbleidao@gmail.com>
2022-03-10 00:50:11 -0300

    basic structure for article and blog models

---
+++ database/migrations/2022_03_10_031840_create_articles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('url');
+            $table->string('imageUrl');
+            $table->string('newsSite');
+            $table->text('summary');
+            $table->dateTime('publishedAt');
+            $table->dateTime('updatedAt');
+            $table->boolean('featured');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function () {
+    return response('Space Flight News', 200);
+});
+
+Route::apiResource('articles', ArticleController::class);
+Route::apiResource('blogs', BlogController::class);
